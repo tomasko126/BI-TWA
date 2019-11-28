2497792eaf06d2a2e7a19bb682e6b8491c93e8f3
Tomáš Taro <tomas@getadblock.com>
2019-11-28 19:04:10 +0100

    Return 204, if no accounts were found

--- src/Controller/Rest/AccountRestController.php
+++ src/Controller/Rest/AccountRestController.php
@@ -4,14 +4,10 @@ namespace App\Controller\Rest;
 
 use App\Services\AccountService;
 use FOS\RestBundle\Controller\AbstractFOSRestController;
-use App\Entity\Employee;
 use App\Services\EmployeeService;
-use Doctrine\DBAL\DBALException;
 use Doctrine\ORM\EntityNotFoundException;
-use Doctrine\ORM\ORMException;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use FOS\RestBundle\View\View;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Serializer\Encoder\JsonEncoder;
 use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
@@ -24,7 +20,7 @@ class AccountRestController extends AbstractFOSRestController {
     /** @var AccountService */
     private $accountService;
 
-
+    /** @var EmployeeService */
     private $employeeService;
 
     public function __construct(AccountService $accountService, EmployeeService $employeeService) {
@@ -46,6 +42,10 @@ class AccountRestController extends AbstractFOSRestController {
         }
 
         $accounts = $employee->getAccounts();
+
+        if ($accounts->isEmpty()) {
+            return $this->view($accounts, Response::HTTP_NO_CONTENT);
+        }
 
         return $this->view($accounts, Response::HTTP_OK);
     }
